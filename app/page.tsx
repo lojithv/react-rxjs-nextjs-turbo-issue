c36739a59c7be5bed7bde7d55795be6336dda47c
lojith vinsuka <lojithvinsuka1998@gmail.com>
2023-05-21 00:48:43 +0530

    Create a hook from an observable

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,46 @@
+"use client"
+import { createSignal } from "@react-rxjs/utils";
+import { map } from "rxjs/operators";
+import { bind, Subscribe } from "@react-rxjs/core";
+
 /** Add your relevant code here for the issue to reproduce */
 export default function Home() {
-  return null
+  return (
+    <div>
+      <Subscribe>
+        <TextInput />
+        <CharacterCount />
+      </Subscribe>
+    </div>
+  );
+}
+
+// A signal is an entry point to react-rxjs. It's equivalent to using a subject
+const [textChange$, setText] = createSignal<any>();
+
+const [useText, text$] = bind<any>(textChange$, "");
+
+function TextInput() {
+  const text = useText();
+
+  return (
+    <div>
+      <input
+        type="text"
+        value={text}
+        placeholder="Type something..."
+        onChange={(e) => setText(e.target.value)}
+      />
+      <br />
+      Echo: {text}
+    </div>
+  );
+}
+
+const [useCharCount, charCount$] = bind(text$.pipe(map((text) => text.length)));
+
+function CharacterCount() {
+  const count = useCharCount();
+
+  return <>Character Count: {count}</>;
 }
